1d5ea2729a36246ab5f2f220d4972a5d4e7a3552
Robin Jalandoni <3dpandabin@gmail.com>
2022-09-10 11:01:56 -0400

    feat: change navbar with new controls

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import { createEventDispatcher, onMount } from 'svelte';
-  import Pedestrian from 'carbon-icons-svelte/lib/Pedestrian.svelte';
-  import Home from 'carbon-icons-svelte/lib/Home.svelte';
+  import { Pedestrian, Home, CalendarHeatMap, Map } from 'carbon-icons-svelte';
   import { Tag } from 'carbon-components-svelte';
   import { isRegistered, userIsSignedIn } from '../stores/publishers';
+  import { page } from '$app/stores';
 
   export let publisherName: string;
   export let adminStatus = false;
@@ -20,52 +20,88 @@
   const logoutUser = () => {
     dispatch('loggedOut');
   };
+
+  const selectedRoute = (routeId: string) => {
+    return $page.routeId === routeId ? 'border-bottom: 2px solid black' : '';
+  };
 </script>
 
-<div class="btn-group">
-  <div id="logout-user" class="btn" on:click={logoutUser}>
-    {#if !$isRegistered || !$userIsSignedIn}
-      <Home size={24} />
-    {:else}
-      <Pedestrian size={24} />
+<div class="container">
+  <div class="btn-group">
+    <div id="logout-user" on:click={logoutUser}>
+      {#if !$isRegistered || !$userIsSignedIn}
+        <div class="btn">
+          <Home size={24} />
+        </div>
+      {:else}
+        <div class="btn">
+          <Pedestrian size={20} />
+          <p>Logout</p>
+        </div>
+      {/if}
+    </div>
+
+    {#if $isRegistered && $userIsSignedIn}
+      <div class="locators">
+        <div class="btn" style={selectedRoute('shifts')}>
+          <CalendarHeatMap size={20} />
+          <p>Shifts</p>
+        </div>
+      </div>
+      <div class="locators">
+        <div class="btn">
+          <Map size={20} />
+          <p>Locations</p>
+        </div>
+      </div>
     {/if}
   </div>
+
   {#if displayTag}
-    {#if adminStatus}
-      <div class="btn admin-tag">
+    <div class="name-tag">
+      {#if adminStatus}
         <Tag type="green">Admin</Tag>
-      </div>
-    {:else}
-      <div class="btn publisher-tag">
+      {:else}
         <Tag type="cyan">{publisherName}</Tag>
-      </div>
-    {/if}
+      {/if}
+    </div>
   {/if}
 </div>
 
 <style>
-  .btn-group {
-    position: fixed;
-    top: 0;
-    display: flex;
-    justify-content: space-between;
+  .container {
     width: 100%;
-    transition: 0.2s;
+    height: 50px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
+
   .btn {
-    margin: 15px;
-  }
-  #logout-user {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px;
     cursor: pointer;
-    padding: 5px;
-    border-radius: 10px;
     transition: 0.2s;
+    border-bottom: var(--is-selected);
   }
-  #logout-user:hover {
-    transform: scale(1.1);
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+  .btn p {
+    font-size: 12px;
   }
-  .admin-tag {
+
+  .btn-group {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .btn:hover {
+    border-bottom: 2px solid black;
+  }
+
+  .name-tag {
+    margin: 8px;
     align-self: flex-end;
   }
 </style>
