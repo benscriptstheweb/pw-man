28e3fa76bb2f7fead269e5c9a44fb2fbf24e1a27
Robin Jalandoni <3dpandabin@gmail.com>
2022-09-14 21:42:24 -0400

    add logic to reroute logged in user back to /shifts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
   import SignOn from '../components/SignOn.svelte';
   import { goto } from '$app/navigation';
+  import { auth } from '../stores/auth';
+  import { onMount } from 'svelte';
+  import { loading } from '../stores/publishers';
+  import { Loading } from 'carbon-components-svelte';
 
   const routeToShifts = () => {
     goto('/shifts');
   };
+
+  onMount(() => {
+    $loading = true;
+    
+    if ($auth.currentUser) {
+      routeToShifts();
+    }
+
+    $loading = false;
+  });
 </script>
 
-<SignOn on:signedInWithAuth={routeToShifts} />
+{#if $loading}
+  <Loading />
+{:else if !$loading}
+  <SignOn on:signedInWithAuth={routeToShifts} />
+{/if}
