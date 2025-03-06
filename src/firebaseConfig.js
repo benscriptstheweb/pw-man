42ada28761dc98b284422c89baeaaaf3b11f2aeb
robin <3dpandabin@gmail.com>
2025-03-06 18:40:03 -0500

    'test'

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyC9Sk-yM-zR_06a4RQf73pVGZOVPW12UhQ',
-  authDomain: 'cart-io.firebaseapp.com',
-  projectId: 'cart-io',
-  storageBucket: 'cart-io.appspot.com',
-  messagingSenderId: '631621845852',
-  appId: '1:631621845852:web:f3d70868848cc860fa3799',
-  measurementId: 'G-0G8X5XHSEZ',
-};
